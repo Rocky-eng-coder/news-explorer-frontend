34bacd3b8c974a1dd464b4e3540d322fe2e9757f
Rockyroad <Racquelbryanteng@email.com>
2025-08-09 20:16:17 -0400

    changes to news explorer application

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,30 +8,36 @@ function Footer() {
       <p className="footer__copyright">
         &copy; News Explorer, Powered by News API
       </p>
-      <nav className="footer__nav">
-        <a href="/" className="footer__link">
-          Home
-        </a>
-        <a
-          href="https://tripleten.com"
-          className="footer__link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          TripleTen
-        </a>
-      </nav>
-      <div className="footer__social">
-        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
-          <img src={githubIcon} alt="GitHub" className="footer__icon" />
-        </a>
-        <a
-          href="https://facebook.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={facebookIcon} alt="Facebook" className="footer__icon" />
-        </a>
+      <div className="footer__right">
+        <nav className="footer__nav">
+          <a href="/" className="footer__link">
+            Home
+          </a>
+          <a
+            href="https://tripleten.com"
+            className="footer__link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            TripleTen
+          </a>
+        </nav>
+        <div className="footer__social">
+          <a
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={githubIcon} alt="GitHub" className="footer__icon" />
+          </a>
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={facebookIcon} alt="Facebook" className="footer__icon" />
+          </a>
+        </div>
       </div>
     </footer>
   );
