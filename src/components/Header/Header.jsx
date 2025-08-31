80c53fb55ae69740986e1be810966e14e16f5976
Rockyroad <Racquelbryanteng@email.com>
2025-08-30 20:00:01 -0400

    changes to news explorer project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
-import LogoutIcon from "../../assets/images/usernameLogout.svg";
 
 function Header({ onSignInClick, onLogout, isLoggedIn, username }) {
   return (
     <header className="header">
-      <Link to="/" className=" header__logo">
+      <Link to="/" className="header__logo">
         NewsExplorer
       </Link>
 
@@ -19,7 +18,6 @@ function Header({ onSignInClick, onLogout, isLoggedIn, username }) {
             Saved articles
           </Link>
         )}
-
         {isLoggedIn ? (
           <button className="header__user-button" onClick={onLogout}>
             <span className="header__username">{username}</span>
